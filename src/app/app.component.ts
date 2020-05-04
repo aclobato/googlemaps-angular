ab0ea7a9288292dbb64176f71e343cc7cae528ab
Ana Carolina Lobato <carolina.lobato@dtidigital.com.br>
2020-05-04 12:14:35 -0300

    Change to Angular 9 and add drawing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
+/// <reference types="@types/googlemaps" />
+
 import { Component } from '@angular/core';
 import { ViewChild } from '@angular/core';
-import { } from '@types/googlemaps';
 
 @Component({
   selector: 'app-root',
@@ -10,26 +11,12 @@ import { } from '@types/googlemaps';
 export class AppComponent {
   @ViewChild('gmap') gmapElement: any;
   map: google.maps.Map;
+  drawingManager: google.maps.drawing.DrawingManager;
 
   latitude: any;
   longitude: any;
 
   iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
-
-  markerTypes = [
-    {
-      text: "Parking", value: "parking_lot_maps.png"
-    }
-    // ,
-    // {
-    //   text: "Library", value: "library_maps.png"
-    // },
-    // {
-    //   text: "Information", value: "info-i_maps.png"
-    // }
-  ];
-
-  selectedMarkerType: string = "parking_lot_maps.png";
 
   isHidden = false;
 
@@ -37,7 +24,7 @@ export class AppComponent {
 
   }
 
-  ngAfterContentInit() {
+  ngAfterViewInit() {
     let mapProp = {
       center: new google.maps.LatLng(18.5793, 73.8143),
       zoom: 15,
@@ -45,10 +32,39 @@ export class AppComponent {
     };
     this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
 
+    this.drawingManager = new google.maps.drawing.DrawingManager({
+      drawingMode: google.maps.drawing.OverlayType.MARKER,
+      drawingControl: true,
+      drawingControlOptions: {
+        position: google.maps.ControlPosition.TOP_CENTER,
+        drawingModes: [google.maps.drawing.OverlayType.MARKER, google.maps.drawing.OverlayType.CIRCLE, google.maps.drawing.OverlayType.POLYGON,
+          google.maps.drawing.OverlayType.POLYLINE, google.maps.drawing.OverlayType.RECTANGLE]
+      },
+      circleOptions: {
+        fillColor: '#ffff00',
+        fillOpacity: 1,
+        strokeWeight: 5,
+        clickable: false,
+        editable: true,
+        zIndex: 1
+      },
+    });
+
+    this.drawingManager.setMap(this.map);
+
+    google.maps.event.addListener(this.drawingManager, 'overlaycomplete', function(event) {
+      if (event.type == 'polygon') {
+        console.log(event.overlay.getPath().getArray().toString());
+        alert(google.maps.geometry.spherical.computeArea(event.overlay.getPath()));
+      } else if (event.type == 'polyline') {
+        console.log(event.overlay.getPath().getArray().toString());
+        alert(google.maps.geometry.spherical.computeLength(event.overlay.getPath()));
+      }
+    });
   }
 
   setMapType(mapTypeId: string) {
-    this.map.setMapTypeId(mapTypeId)
+    this.map.setMapTypeId(mapTypeId);
   }
 
   setCenter() {
@@ -77,26 +93,7 @@ export class AppComponent {
     alert('Marker\'s Title: ' + marker.getTitle());
   }
 
-  showCustomMarker() {
-
-
-    this.map.setCenter(new google.maps.LatLng(this.latitude, this.longitude));
-
-    let location = new google.maps.LatLng(this.latitude, this.longitude);
-
-    console.log(`selected marker: ${this.selectedMarkerType}`);
-
-    let marker = new google.maps.Marker({
-      position: location,
-      map: this.map,
-      icon: this.iconBase + this.selectedMarkerType,
-      title: 'Got you!'
-    });
-  }
-
-  toggleMap() {
-    this.isHidden = !this.isHidden;
-
-    this.gmapElement.nativeElement.hidden = this.isHidden;
+  setPolygonMode() {
+    this.drawingManager.setDrawingMode(google.maps.drawing.OverlayType.POLYGON);
   }
 }
